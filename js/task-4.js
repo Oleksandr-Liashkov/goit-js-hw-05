0197ac849872395e6d5c953d16da2c7c8c440640
Oleksandr Liashkov <aleksandr.liashkov98@gmail.com>
2025-11-22 21:58:13 +0200

    Fourth Task done

--- js/task-4.js
+++ js/task-4.js
@@ -1,3 +1,8 @@
+const getTotalBalanceByGender = (users, gender) =>
+  users
+    .filter(user => user.gender === gender)
+    .map(user => user.balance)
+    .reduce((total, balance) => (total += balance), 0);
 const clients = [
   {
     name: 'Moore Hensley',
